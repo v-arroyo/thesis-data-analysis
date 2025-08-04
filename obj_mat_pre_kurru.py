03991a9e962d96981ec989cd48f7b3edcfee2024
Victoria <victoria@localhost.localdomain>
2025-08-04 14:05:43 +0200

    new folder more oganization

---
+++ obj_mat_pre_kurru.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import plotly.express as px
+from sqlalchemy import create_engine
+import plotly.io as pio
+
+pio.defaults.chromium_executable = '/usr/bin/chromium'
+
+engine = create_engine(f'mysql+pymysql://{os.getenv"DB_USER")}:{os.getenv("DB_PASSWORD")}@localhost/{os.getenv("DB_NAME")}')
+
+query = """
+SELECT 
+    s.site_name,
+    b.owner,
+    CASE 
+        WHEN a.artifact_material = 'lapis' THEN 'lapis lazuli'
+        WHEN a.artifact_material = 'sandalwood' THEN 'wood'
+        WHEN a.artifact_material = 'cowrie shell' THEN 'shell'
+        WHEN a.artifact_material = 'snail shell' THEN 'shell'
+        ELSE a.artifact_material
+    END AS material,
+    COUNT(artifact_id) as count
+FROM burials b
+JOIN sites s
+ON s.site_id = b.site_id
+JOIN artifacts a
+ON a.burial_id = b.burial_id
+WHERE temp = 'pre-25th dyn.' AND b.site_id IN (1) AND a.artifact_material IS NOT NULL
+GROUP BY 1,2,3
+"""
+
+df = pd.read_sql(query, engine)
+
+custom_colors = ['#e9724d', '#92cad1', '#d6d727', '#79ccb3', '#868686']
+
+fig = px.scatter(
+    df,
+    y="material",
+    x="count",
+    color="owner",
+    symbol="owner",
+    facet_col='site_name',
+    title="Early Napatan object materials",
+    labels={"owner": "owner", "site_name": "site"},
+    color_discrete_sequence=custom_colors,
+    template="plotly_white"
+)
+
+fig.update_layout(yaxis={'categoryorder': 'total ascending'}, 
+    legend=dict(
+        orientation="h",
+        yanchor="top",
+        y=-0.03,
+        xanchor="right",
+        x=0.90,
+        traceorder='reversed'),
+    font=dict(
+        family="Verdana, sans-serif",
+        color='black',
+        size=8),
+    legend_title_text='',
+    margin=dict(l=0, r=5, t=40, b=0),
+    autosize=True,
+    title_font=dict(size=8)
+)
+
+fig.update_xaxes(title_text='')
+fig.update_yaxes(title_text='')
+
+pio.write_image(fig, 'phd_final/images/objects_mat_pre_kurru.png',scale=3, width=450, height=450)
---
+++ objs_pre.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import plotly.express as px
+from sqlalchemy import create_engine
+import plotly.io as pio
+
+engine = create_engine(f'mysql+pymysql://{os.getenv"DB_USER")}:{os.getenv("DB_PASSWORD")}@localhost/{os.getenv("DB_NAME")}')
+
+query = """
+SELECT 
+    s.site_name,
+    b.owner,
+    a.artifact_type,
+    COUNT(artifact_id) as count
+FROM burials b
+JOIN sites s
+ON s.site_id = b.site_id
+JOIN artifacts a
+ON a.burial_id = b.burial_id
+WHERE temp = 'pre-25th dyn.' AND b.site_id IN (1) AND artifact_type NOT IN ('beads', 'shabtis')
+GROUP BY 1,2,3
+"""
+
+df = pd.read_sql(query, engine)
+
+custom_colors = ['#e9724d', '#92cad1', '#d6d727', '#79ccb3', '#868686']
+
+fig = px.bar(
+    df,
+    x="count",
+    y="artifact_type",
+    color="owner",
+    facet_col="site_name",
+    barmode='group',
+    title="Pre-25th Dynasty object types",
+    labels={"count": "total", "owner": "owner", "artifact_type": "obj. type", "site_name": "site"},
+    color_discrete_sequence=custom_colors,
+    template="plotly_white"
+)
+
+fig.update_layout(yaxis={'categoryorder': 'total ascending'}, 
+    legend=dict(
+        orientation="v",
+        yanchor="middle",
+        y=0.50,
+        xanchor="center",
+        x=0.80,
+        traceorder='reversed'),
+    font=dict(
+        family="Verdana, sans-serif",
+        color='black',
+        size=8),
+    legend_title_text='',
+    xaxis=dict(
+        tickmode='linear',
+        dtick=100),
+    margin=dict(l=5, r=10, t=40, b=0),
+    autosize=True,
+    title_font=dict(size=8)
+)
+
+fig.update_traces(textposition='auto')
+fig.update_xaxes(title_text='')
+fig.update_yaxes(title_text='')
+
+pio.write_image(fig, 'phd_final/images/objects_pre_kurru.png',scale=3, width=450, height=300)
